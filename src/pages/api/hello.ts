287f7301af44c4d93754f24ae297202c688ab94f
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2024-06-08 10:07:07 +0530

    asdf

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -12,10 +12,10 @@ export default function handler(
 	res: NextApiResponse<ResponseData>
 ) {
 	res.status(200).json({
-		expoPublicAPIUrl: 'https://strong-seas-make.loca.lt',
+		expoPublicAPIUrl: 'https://cuddly-paws-share.loca.lt',
 		expoPublicAPIKey:
 			'be8bbff65a7683861ff224ee878da27371184b01c56575a6dfc926387c7050e2edab4399397a056b37d29681c53267f13c8b8787aa9a59c8fbd81734ddc29f4c1d228ffd996121a54abf6b47d859dcaef7aea38daf239c7b98d815c70ad86823db429ed9743d10aa64920ce2e530cf45504288cb0ff5f6a1eb1504e171708cc9',
-		heartBeatAPIUrl: 'https://witty-colts-film.loca.lt',
-		tokenAPIUrl: 'https://sour-wings-play.loca.lt',
+		heartBeatAPIUrl: 'https://sixty-pianos-sink.loca.lt',
+		tokenAPIUrl: 'https://lovely-pots-bathe.loca.lt',
 	})
 }
