c8c56a20821ec888488b4b9cb57cb28022cb8d88
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2024-06-08 10:24:37 +0530

    asdf

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -15,7 +15,7 @@ export default function handler(
 		expoPublicAPIUrl: 'https://cuddly-paws-share.loca.lt',
 		expoPublicAPIKey:
 			'be8bbff65a7683861ff224ee878da27371184b01c56575a6dfc926387c7050e2edab4399397a056b37d29681c53267f13c8b8787aa9a59c8fbd81734ddc29f4c1d228ffd996121a54abf6b47d859dcaef7aea38daf239c7b98d815c70ad86823db429ed9743d10aa64920ce2e530cf45504288cb0ff5f6a1eb1504e171708cc9',
-		heartBeatAPIUrl: 'https://hip-actors-thank.loca.lt',
+		heartBeatAPIUrl: 'https://fine-friends-study.loca.lt',
 		tokenAPIUrl: 'https://5830-42-108-75-20.ngrok-free.app',
 	})
 }
